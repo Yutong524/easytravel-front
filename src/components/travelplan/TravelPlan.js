92876828691882ff43a8752277d10ff608da3d28
David He <ameer886@outlook.com>
2024-06-23 11:11:20 -0400

    updated travel plan list

--- src/components/travelplan/TravelPlan.js
+++ src/components/travelplan/TravelPlan.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
-import { Button, List, Empty, message } from 'antd';
+import { Button, List, Empty } from 'antd';
 import './TravelPlan.css';
 import CreateTravelPlan from './CreateTravelPlan';
 
 function TravelPlan({ userId }) {
   const [travelPlans, setTravelPlans] = useState([]);
   const [createPlanModalVisible, setCreatePlanModalVisible] = useState(false);
+  const [showRoutes, setShowRoutes] = useState(false);
 
   const handleCreatePlanClick = () => {
     setCreatePlanModalVisible(true);
@@ -23,43 +24,87 @@ function TravelPlan({ userId }) {
   useEffect(() => {
     if (!userId) return;
 
-    fetch(`/api/travel-plans/${userId}`)
-      .then(response => response.json())
-      .then(data => setTravelPlans(data))
-      .catch(error => {
-        message.error('Failed to fetch travel plans.');
-        console.error(error);
-      });
+    // fetch(`/api/travel-plans/${userId}`)
+    //   .then(response => response.json())
+    //   .then(data => setTravelPlans(data))
+    //   .catch(error => {
+    //     message.error('Failed to fetch travel plans.');
+    //     console.error(error);
+    //   });
+    const plans = [
+      {
+        "id": 1,
+        "planId": 1,
+        "name": "New York",
+        "description": "Going to new york yay",
+        "tags": ["ny", "summer"],
+        "routes": [],
+      },
+      {
+        "id": 2,
+        "planId": 2,
+        "name": "LA",
+        "description": "Going to LA yay",
+        "tags": ["LA", "summer"],
+        "routes": ["Hollywood"]
+      }
+    ];
+    setTravelPlans(plans);
   }, [userId]);
+
+  const viewRoutes = (routes) => {
+    if (routes.length === 0) {
+      return <div>This plan has 0 route. See All Route</div>
+    } else{
+      return <button onClick={toggleView}>Edit</button>
+    }
+  }
+
+  const toggleView = () => {
+    setShowRoutes(true)
+  };
 
   const renderItem = (plan) => {
     return (
-      <List.Item className="plan-item">
-        <h4>{plan.name}</h4>
+      <List.Item className="plan-item" extra={viewRoutes(plan.routes)}>
+        <List.Item.Meta
+          title={plan.name}
+          description={plan.description}
+        />
+        <div className="plan-detail-container">
+          <button>History</button>
+        </div>
       </List.Item>
     );
   };
 
   return (
+    showRoutes ? (<>Routes</>) :
     <div className="travel-plan-container">
-      <Button type="primary" className="create-plan-button" onClick={handleCreatePlanClick}>
-        Create New Plan
-      </Button>
-      <CreateTravelPlan
-        visible={createPlanModalVisible}
-        onClose={handleCloseCreatePlanModal}
-        onCreate={handleCreatePlan}
-        userId={userId}
-      />
-      {travelPlans.length > 0 ? (
-        <List
-          className="plan-list"
-          dataSource={travelPlans}
-          renderItem={renderItem}
+      <div className="create-plan-container">
+        <Button type="primary" className="create-plan-button" onClick={handleCreatePlanClick}>
+          Create New Plan
+        </Button>
+        <CreateTravelPlan
+          visible={createPlanModalVisible}
+          onClose={handleCloseCreatePlanModal}
+          onCreate={handleCreatePlan}
+          userId={userId}
         />
-      ) : (
-        <Empty description="You don't have any plan yet!" className="no-plan-message" />
-      )}
+      </div>
+      <div className="travel-plan-list-container">
+        {travelPlans.length > 0 ? (
+          <List
+            className="plan-list"
+            dataSource={travelPlans}
+            renderItem={renderItem}
+            itemLayout="vertical"
+            bordered
+          />
+        ) : (
+          <Empty description="You don't have any plan yet!" className="no-plan-message" />
+        )}
+      </div>
     </div>
   );
 }
